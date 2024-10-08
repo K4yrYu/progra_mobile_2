e283d07843a0ba27946e996b80c92090c12806eb
K4yrYu <Vicentepalma1202@gmail.com>
2024-10-08 20:40:07 -0300

    eliminado parcial

--- src/app/pages/eliminarconsola/eliminarconsola.page.ts
+++ src/app/pages/eliminarconsola/eliminarconsola.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AlertasService } from 'src/app/services/alertas.service'; // Asegúrate de que la ruta sea correcta
+import { ManejodbService } from 'src/app/services/manejodb.service';
 
 @Component({
   selector: 'app-eliminarconsola',
@@ -9,20 +10,48 @@ import { AlertasService } from 'src/app/services/alertas.service'; // Asegúrate
 })
 export class EliminarconsolaPage implements OnInit {
 
-  constructor(
-    private alertasService: AlertasService, // Inyección del servicio
-    private router: Router
-  ) { }
+  consolaLlego: any;
 
-  ngOnInit() { }
+  arregloConsolaUnico: any = [
+    {
+      id_producto: '',
+      nombre_prod: '',
+      precio_prod: '',
+      stock_prod:  '',
+      descripcion_prod: '',  
+      foto_prod: '',
+      estatus: '',
+      id_categoria: '',
+    }
+  ]
+
+  constructor(private bd: ManejodbService, private router: Router, private activedroute: ActivatedRoute) {
+    this.activedroute.queryParams.subscribe(res=>{
+      if(this.router.getCurrentNavigation()?.extras.state){
+        this.consolaLlego = this.router.getCurrentNavigation()?.extras?.state?.['consolaSelect'];
+      }
+    })
+   }
+
+  ngOnInit() {
+    // verificar si la BD está disponible
+    this.bd.dbState().subscribe(data => {
+      if (data) {
+        // subscribir al observable de la consulta
+        this.bd.fetchConsolaUnico().subscribe(res => {
+          this.arregloConsolaUnico = res;
+          this.bd.consultarConsolaPorId(this.consolaLlego.id_producto);
+        });
+      }
+    });
+  }
 
   async eliminarConsola() {
-    // Aquí puedes realizar la lógica para eliminar la consola
+    // Aquí puedes realizar la lógica para eliminar el videojuego
 
-    // Luego, muestra la alerta
-    await this.alertasService.presentAlert('Producto Eliminado', 'La consola ha sido eliminada exitosamente.');
+    await this.bd.eliminarConsola(this.consolaLlego.id_producto);
 
     // Redirige después de que el usuario haya cerrado la alerta
-    this.router.navigate(['/crudconsolas']);
+    await this.router.navigate(['/crudconsolas']);
   }
 }
