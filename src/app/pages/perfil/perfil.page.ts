bdb69129cd5463ff9f552303289d07bef12ad723
Alonso <amuc23@gmail.com>
2024-10-10 17:41:05 -0300

    cambios a foto pre definida en perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -11,8 +11,8 @@ import { ManejodbService } from 'src/app/services/manejodb.service';
 })
 export class PerfilPage implements OnInit {
 
-
-  arregloUsuarioConectado: any [] = [
+  // Definir la variable que contendrá los datos del usuario conectado
+  arregloUsuarioConectado: any[] = [
     {
       id_usuario: '',
       rut_usuario: '',
@@ -27,41 +27,50 @@ export class PerfilPage implements OnInit {
       userlogged: '',
       id_rol: ''
     }
-  ]
+  ];
 
-
-
-  constructor
-  (private alertasService: AlertasService,
-   private bd: ManejodbService,
-   private router: Router,
-   private platform: Platform // Inyecta Platform
-   ) {}
+  constructor(
+    private alertasService: AlertasService,
+    private bd: ManejodbService,
+    private router: Router,
+    private platform: Platform // Inyecta Platform
+  ) {}
 
   ngOnInit() {
+    // Verificar si la base de datos está lista
     this.bd.dbState().subscribe(data => {
       if (data) {
-        // Llama a la función que consulta todos los usuarios por estado
-        this.bd.consultarUsuariosPorEstadoConectado().then(res => {
-          this.arregloUsuarioConectado = res; // Almacena los usuarios en la variable
-        }).catch(error => {
-          console.error('Error al consultar usuarios por estado:', error);
-        });
+        this.consultarUsuarios();
       }
     });
 
-     // Suscripción al evento de cambio de ubicación de la página
-     this.platform.backButton.subscribeWithPriority(10, () => {
-      // Lógica para cerrar sesión al presionar el botón de retroceso del teléfono
-      this.cerrarSesion();
+    // Manejar el evento de botón "back" del dispositivo
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      this.cerrarSesion(); // Lógica para cerrar sesión al presionar el botón de retroceso
     });
   }
 
- 
+  // Función para consultar los usuarios conectados
+  async consultarUsuarios() {
+    try {
+      this.arregloUsuarioConectado = await this.bd.consultarUsuariosPorEstadoConectado();
+    } catch (error) {
+      console.error('Error al consultar usuarios por estado:', error);
+    }
+  }
 
- 
+  // Retorna la imagen del usuario o una imagen predeterminada si no existe
+  getImagenUsuario(foto: string | null): string {
+    return foto ? foto : 'assets/img/user_default_photo.jpg';
+  }
+
+  // Función para cerrar la sesión y redirigir al login
   async cerrarSesion() {
-    await this.bd.cerrarSesion();
-    this.router.navigate(['/login']);
+    try {
+      await this.bd.cerrarSesion();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
   }
 }
